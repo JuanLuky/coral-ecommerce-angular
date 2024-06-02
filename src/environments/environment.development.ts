5d2b012203ceeb33ec10861d62fd562bcb906754
Juan Carlos <juandev33@gmail.com>
2024-06-02 20:50:38 -0300

    bug: correção no caminho da api de usuário de produtos

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api: 'https://coral-render-app.onrender.com/shop',
-  user: 'http://localhost:8080/auth'
+  api: 'https://coral-render-app.onrender.com/api/v1/shops',
+  user: 'https://coral-render-app.onrender.com/auth'
 };
